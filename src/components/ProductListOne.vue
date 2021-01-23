84aec4862f2dfea4d8340eb8f91da8eb5e5ec1f1
Ihsan <muhammadihsanansory@gmail.com>
2021-01-23 08:32:28 +0700

    yes

---
+++ src/components/ProductListOne.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="product-list-one">
+    <h2>Product List One</h2>
+    <ul>
+      <li v-for="(product, i) in products" :key="i">
+        <span class="name">{{ product.name }}</span>
+        =
+        <span class="size">{{ product.size }} GB</span>
+      </li>
+    </ul>
+    <button @click="reduceSize(2)">Reduce Size</button>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+
+export default {
+  computed: {
+    products() {
+      return this.$store.state.products;
+    },
+    ...mapGetters(["saleProducts"]),
+  },
+  methods: {
+    ...mapActions(["reduceSize"]),
+  },
+};
+</script>
+
+
+<style scoped>
+#product-list-one {
+  background: #fff8b1;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
+  margin-bottom: 30px;
+  padding: 10px 20px;
+}
+#product-list-one ul {
+  padding: 0;
+}
+#product-list-one li {
+  display: inline-block;
+  margin-right: 10px;
+  margin-top: 10px;
+  padding: 20px;
+  background: rgba(255, 255, 255, 0.7);
+}
+.price {
+  font-weight: bold;
+  color: #e8800c;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,52 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    products: [{
+        name: "Point Blank",
+        size: 3
+      },
+      {
+        name: "Apex Legend",
+        size: 28
+      },
+      {
+        name: "PUBG",
+        size: 12
+      },
+      {
+        name: "PES 2020",
+        size: 31
+      },
+      {
+        name: "Stronghold Crusader II",
+        size: 16
+      },
+    ],
+  },
+  getters: {
+    saleProducts: state => {
+      let saleProducts = state.products.map((product) => {
+        return {
+          name: `**${product.name}**`,
+          size: product.size * 3,
+        };
+      });
+      return saleProducts;
+    }
   },
   mutations: {
+    reduceSize: (state, payload) => {
+      state.products.forEach((product) => {
+        product.size -= payload;
+      });
+    }
   },
   actions: {
+    reduceSize: (context, payload) => {
+      setTimeout(function () {
+        context.commit('reduceSize', payload)
+      }, 1000)
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
